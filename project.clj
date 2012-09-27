27b334222636ed9660eedd676f01db9b224f0677
August Lilleaas <august@augustl.com>
2012-09-27 09:28:14 +0200

    Adding deploy repos.

--- project.clj
+++ project.clj
@@ -2,4 +2,7 @@
   :description "Multi-environment ring handlers"
   :license {:name "New BSD license"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:test {:resource-paths ["test/resources"]}})
+  :profiles {:test {:resource-paths ["test/resources"]}}
+  :deploy-repositories
+  {"releases" {:url "http://augustl.com:8081/nexus/content/repositories/releases"}
+   "snapshots" {:url "http://augustl.com:8081/nexus/content/repositories/snapshots"}})

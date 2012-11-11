8b59aae8a6a6e23ff6baab6750c6eef8dee7e3db
August Lilleaas <august@augustl.com>
2012-11-11 02:19:32 +0100

    Adding url to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject oiiku-ring-env "0.1.0"
   :description "Multi-environment ring handlers"
+  :url "https://github.com/oiiku/oiiku-ring-env"
   :license {:name "New BSD license"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:test {:resource-paths ["test/resources"]}})

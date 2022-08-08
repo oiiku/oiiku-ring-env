d4b391256cb1035e5936f5d99c89bf61d3a93804
Emil Bengtsson <emil@emil0r.com>
2022-08-08 15:19:28 +0200

    Upgrade deps
    Upgrade Clojure deps to 1.11.1 (latest at the time of writing).

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject oiiku-ring-env "0.3.0"
+(defproject oiiku-ring-env "0.4.0-SNAPSHOT"
   :description "Multi-environment ring handlers"
   :url "https://github.com/oiiku/oiiku-ring-env"
   :license {:name "New BSD license"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :profiles {:test {:resource-paths ["test/resources"]}})

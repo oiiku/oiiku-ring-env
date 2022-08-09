a0ce76d1190024df330b532fd4b5d747329889e7
Emil Bengtsson <emil@emil0r.com>
2022-08-09 10:42:02 +0200

    Add Nexus repos

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "https://github.com/oiiku/oiiku-ring-env"
   :license {:name "New BSD license"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
-  :profiles {:test {:resource-paths ["test/resources"]}})
+  :profiles {:test {:resource-paths ["test/resources"]}}
+  :repositories
+  {"oiiku-releases" "https://nexus.oiiku.no/nexus/content/repositories/releases"
+   "oiiku-snapshots" "https://nexus.oiiku.no/nexus/content/repositories/snapshots"}
+  :deploy-repositories
+  {"releases" {:url "https://nexus.oiiku.no/nexus/content/repositories/releases"}
+   "snapshots" {:url "https://nexus.oiiku.no/nexus/content/repositories/snapshots"}})
